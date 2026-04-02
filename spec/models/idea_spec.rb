53d9ca8f2b575cd5c7fb65f39e923a026bf0e4a9
saekoy <saeconomail@gmail.com>
2026-04-02 16:25:31 +0900

    Ideaモデルのテストを追加

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Idea, type: :model do
+  describe 'バリデーション' do
+    it 'title と category があれば有効である' do
+      idea = Idea.new(title: '残業あるある', category: 'トーク・雑談')
+      expect(idea).to be_valid
+    end
+
+    it 'title が空では無効である' do
+      idea = Idea.new(title: '', category: 'トーク・雑談')
+      expect(idea).not_to be_valid
+    end
+
+    it 'category が空では無効である' do
+      idea = Idea.new(title: '残業あるある', category: '')
+      expect(idea).not_to be_valid
+    end
+  end
+
+  describe 'デフォルト値' do
+    it 'like_count のデフォルトは 0 である' do
+      idea = Idea.new(title: '残業あるある', category: 'トーク・雑談')
+      expect(idea.like_count).to eq(0)
+    end
+  end
+end
