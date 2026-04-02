09c690aa02a1d09e38831f471a8bb1905a3d322b
saekoy <saeconomail@gmail.com>
2026-04-02 15:37:08 +0900

    POST /ideas で企画10件をJSON返却するcreateアクションを追加

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,9 @@
 class IdeasController < ApplicationController
   def index
   end
+
+  def create
+    dummy_ideas = Array.new(10) { |i| "企画アイデア#{i + 1}" }
+    render json: { ideas: dummy_ideas }
+  end
 end
--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -7,4 +7,34 @@ RSpec.describe 'Ideas', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /ideas' do
+    context '正常なパラメータを送信した場合' do
+      let(:valid_params) do
+        { category: 'トーク・雑談', memo: '今日は残業でクタクタ' }
+      end
+
+      it 'HTTP ステータス 200 を返す' do
+        post '/ideas', params: valid_params
+        expect(response).to have_http_status(200)
+      end
+
+      it 'JSON形式で返す' do
+        post '/ideas', params: valid_params
+        expect(response.content_type).to include('application/json')
+      end
+
+      it 'ideasキーを含む' do
+        post '/ideas', params: valid_params
+        json = JSON.parse(response.body)
+        expect(json).to have_key('ideas')
+      end
+
+      it 'ideas が10件返る' do
+        post '/ideas', params: valid_params
+        json = JSON.parse(response.body)
+        expect(json['ideas'].length).to eq(10)
+      end
+    end
+  end
 end
